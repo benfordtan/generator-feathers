3daf00cc7e1611b1e8ee6494b2bb9a4135c28808
Benford Tan <benford.tan@outlook.com>
2021-06-14 18:22:19 +0800

    Fix email type for user to prevent conversion error

--- generators/service/templates/ts/schema/mongoose-user.ts
+++ generators/service/templates/ts/schema/mongoose-user.ts
@@ -1,7 +1,7 @@
 export default {
   'type': 'object',
   'properties': {<% if(authentication.strategies.indexOf('local') !== -1) { %>
-    'email': { 'type': ['string', 'null'] },
+    'email': { 'type': 'string' },
     'password': { 'type': 'string' },<% } %><% authentication.oauthProviders.forEach(provider => { %>
     '<%= provider %>Id': { 'type': 'string' },<% }); %>
   },
