29aa4cfc3bb860d8a994f41ec495a647873a6476
Benford Tan <benford.tan@outlook.com>
2021-06-16 14:46:37 +0800

    Add semicolons

--- generators/service/templates/ts/schema-user.ts
+++ generators/service/templates/ts/schema-user.ts
@@ -6,4 +6,4 @@ export default {
     '<%= provider %>Id': { 'type': 'string' },<% }); %>
   },
   'required': ['password']
-}
+};
--- generators/service/templates/ts/schema.ts
+++ generators/service/templates/ts/schema.ts
@@ -6,4 +6,4 @@ export default {
     }
   },
   'required': ['text']
-}
+};
